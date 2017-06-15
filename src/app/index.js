22e019f6763d202d8f936c6d12863948be3f6b46
Ana Raquel Campesan <anazard@outlook.com>
2017-06-15 14:20:13 -0400

    added new nested components

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+
 var app = window.document.getElementById('app');
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello world!</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Header/>
+          </div>
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home/>
+          </div>
+        </div>
       </div>
     );
   }
